7d95978989ce374221cf96009b6473feea60b0a6
maywoollcott <maywoollcott@gmail.com>
2022-02-09 19:53:04 -0500

    fix email validation

--- utils/emailValidation.tsx
+++ utils/emailValidation.tsx
@@ -1,8 +1,3 @@
 export const validateEmail = (email: string) => {
-  const validFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-  if (email.match(validFormat)) {
-    return true;
-  } else {
-    return false;
-  }
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
